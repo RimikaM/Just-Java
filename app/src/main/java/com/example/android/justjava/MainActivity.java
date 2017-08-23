321b05f2b4363c352d0f454c6524dea22cbad84d
RimikaM <rimikamajumdar@Rimikas-MacBook-Pro.local>
2017-08-23 13:43:59 -0700

    added display price functionality

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.justjava;
 
+import android.icu.text.NumberFormat;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,8 +9,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    /* app displays an order form to order coffee
-     */
+    /* app displays an order form to order coffee */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +19,21 @@ public class MainActivity extends AppCompatActivity {
     /* method called when order button is clicked */
     public void submitOrder(View view) {
         display(1);
+        displayPrice(2*5);
     }
 
     /* method displays the given quantity values */
     private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        TextView quantityTextView = (TextView)
+                findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    /* method displays given price */
+    private void displayPrice(int number){
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        }
     }
 }
