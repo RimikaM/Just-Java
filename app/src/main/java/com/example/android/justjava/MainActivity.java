bfe9e80a638af990b36ff367c55a0f88483a8eb6
RimikaM <rimikamajumdar@Rimikas-MacBook-Pro.local>
2017-08-22 18:03:05 -0700

    added code for the button functionality

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.android.justjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    /* app displays an order form to order coffee
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /* method called when order button is clicked */
+    public void submitOrder(View view) {
+        display(1);
+    }
+
+    /* method displays the given quantity values */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
     }
 }
