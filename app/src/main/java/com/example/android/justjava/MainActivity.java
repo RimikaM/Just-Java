2d2a68e36c5636f27ebff56161395e85637553f5
RimikaM <rimikamajumdar@Rimikas-MacBook-Pro.local>
2017-08-28 12:10:12 -0700

    Done.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -20,7 +20,9 @@ public class MainActivity extends AppCompatActivity {
 
     /* method called when order button is clicked */
     public void submitOrder(View view) {
-        String priceMessage = "Free";
+        int price = quantity * 5;
+        String priceMessage = "Total: $" + price;
+        priceMessage = priceMessage + "\nThank you!";
         displayMessage(priceMessage);
     }
 
